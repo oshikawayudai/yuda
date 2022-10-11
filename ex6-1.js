ba907d55e072539b6044f71477511cb8da364cbf
押川憂大 <103973958+oshikawayudai@users.noreply.github.com>
2022-10-11 10:43:03 +0900

    Update ex6-1.js

--- ex6-1.js
+++ ex6-1.js
@@ -5,3 +5,13 @@ function sayhello61(){
 function saygoodbye(){
  alert('Goodbye.');
 }
+
+var who65 = 'world';
+function sayhello65() {
+alert('Hello, ' + who65 + '!');
+}
+function taro65() {
+who65 = '太郎';
+}
+function hanako65() {
+who65 = '花子';
