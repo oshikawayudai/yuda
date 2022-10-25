779dffa4b639a1928637f8a4f1a0ccb4fdcb9e45
押川憂大 <103973958+oshikawayudai@users.noreply.github.com>
2022-10-25 09:42:53 +0900

    Update ex6-7.js

--- ex6-7.js
+++ ex6-7.js
@@ -1,3 +1,9 @@
 var who67='world';
 function sayhello67(){alert('Hello,'+who67+'!');}
 function someone(x){who67=x;}
+var who67 = 'world';
+function sayhello67(){
+alert('Hello, ' + who67 + '!');
+}
+function someone(x) {
+who67 = x;
